cb170d6bd1ee0dfb052ad9ec14096e296eec6c3c
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2023-01-03 01:54:07 +0100

    Initial Commit

---
+++ DrawSimulator/DrawSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawSimulatorApp.swift
+//  DrawSimulator
+//
+//  Created by Arthur Falque Pierrotin on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
