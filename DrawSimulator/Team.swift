410d806503346799658f12836177b0a1a5f445f4
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2023-03-19 15:19:45 +0100

    Adding Team struct and teams json

---
+++ DrawSimulator/Team.swift
@@ -0,0 +1,14 @@
+//
+//  Team.swift
+//  DrawSimulator
+//
+//  Created by Arthur Falque Pierrotin on 19/03/2023.
+//
+
+import Foundation
+
+struct Team {
+    var id: Int
+    let name: String
+    let shortName: String
+}
