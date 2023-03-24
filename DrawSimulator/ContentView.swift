20b44d3a310e624c38598a8c8af87aaec44c7716
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2023-03-24 16:10:57 +0100

    Breaking ContentView with a sub view to prepare for the team grouping by countries

--- DrawSimulator/ContentView.swift
+++ DrawSimulator/ContentView.swift
@@ -11,42 +11,10 @@ struct ContentView: View {
     
     let teams: [Team] = Bundle.main.jsonDecode("teams.json")
     
-    var teamsByPool: [[Team]] {
-        Array(Dictionary(grouping: teams, by: { $0.pool }).values).sorted {
-            $0[0].pool < $1[0].pool
-        }
-    }
-    
     var body: some View {
         NavigationView {
-            GeometryReader { geo in
-                
-                let imgSize = geo.size.width * 0.12
-                
-                List(teamsByPool, id:\.self) { teams in
-                    
-                    Section(teams.first!.pool) {
-                        
-                        ForEach(teams) { team in
-                            
-                            NavigationLink {
-                                Text(team.name)
-                            } label: {
-                                HStack {
-                                    Image(team.name)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: imgSize, height: imgSize)
-                                    
-                                    Text(team.name)
-                                        .font(.title)
-                                        .padding(.leading)
-                                }
-                            }
-                        }
-                    }
-                }
-                .listStyle(GroupedListStyle())
+            VStack {
+                TeamsByPoolView(teams: teams)
             }
             .navigationTitle("UEFA CL Draw")
         }
