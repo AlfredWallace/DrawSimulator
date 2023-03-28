cc64db10dd3bf044aaf9116ff64bca962c4d3696
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2023-03-28 17:27:23 +0200

    Removing subview and use computed properties for the two groupings

--- DrawSimulator/ContentView.swift
+++ DrawSimulator/ContentView.swift
@@ -10,13 +10,103 @@ import SwiftUI
 struct ContentView: View {
     
     let teams: [Team] = Bundle.main.jsonDecode("teams.json")
+    let countries: [Country] = Bundle.main.jsonDecode("countries.json")
+    
+    private var countriesDict: [Int: Country] {
+        var result = [Int: Country]()
+        
+        countries.forEach {
+            result[$0.id] = $0
+        }
+        
+        return result
+    }
+    
+    enum Grouping {
+        case pool, country
+    }
+    
+    @State private var grouping = Grouping.pool
+    
+    private var groupingIconName: String {
+        switch grouping {
+            case .country:
+                return "flag.square"
+            default:
+                return "list.dash"
+        }
+    }
+    
+    private var groupedTeams: [[Team]] {
+        
+        if grouping == .pool {
+            return Array(Dictionary(grouping: teams, by: { $0.pool }).values).sorted {
+                $0[0].pool < $1[0].pool
+            }
+        }
+        
+        return Array(Dictionary(grouping: teams, by: { $0.countryId }).values).sorted {
+            if $0.count == $1.count {
+                return countriesDict[$0[0].countryId]!.name < countriesDict[$1[0].countryId]!.name
+            }
+            
+            return $0.count > $1.count
+            
+        }
+    }
     
     var body: some View {
         NavigationView {
             VStack {
-                TeamsByPoolView(teams: teams)
+                GeometryReader { geo in
+                    
+                    let imgSize = geo.size.width * 0.12
+                    
+                    List(groupedTeams, id:\.self) { teams in
+                        
+                        Section(grouping == .pool ? teams.first!.pool : countriesDict[teams.first!.countryId]!.name) {
+                            
+                            ForEach(teams) { team in
+                                
+                                NavigationLink {
+                                    Text(team.name)
+                                } label: {
+                                    HStack {
+                                        
+                                        Image(team.name)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: imgSize, height: imgSize)
+                                        
+                                        Text(team.name)
+                                            .font(.title)
+                                            .padding(.leading)
+                                        
+                                        Spacer()
+                                        
+                                        // todo: use this in the "by country" view later
+                                        //                                    Image(systemName: "\(team.seeded ? "1" : "2").circle")
+                                        //                                        .font(.title3)
+                                        //                                        .foregroundColor(.gray)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .listStyle(GroupedListStyle())
+                }
             }
             .navigationTitle("UEFA CL Draw")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        grouping = grouping == .country ? .pool : .country
+                    } label: {
+                        Label("Grouping", systemImage: groupingIconName)
+                            .font(.title2)
+                    }
+                }
+            }
         }
     }
 }
