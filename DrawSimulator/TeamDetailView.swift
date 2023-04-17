a4ac10a7969f08ce48ae1e58d3c43e4b08d1b03b
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2023-04-18 01:08:35 +0200

    Changing data structure for the pairings in the team detail view

--- DrawSimulator/TeamDetailView.swift
+++ DrawSimulator/TeamDetailView.swift
@@ -15,24 +15,41 @@ struct TeamDetailView: View {
     
     let team: Team
     
-    var pairings: [Draws.Pairing] {
-        draws.pairings.filter { pairing in
-            if team.seeded {
-                return pairing.seededTeam == team
-            } else {
-                return pairing.unseededTeam == team
+    private var opponents: [Team] {
+        SharedConstants.teams.filter { opponent in
+            opponent.seeded != team.seeded
+            && opponent.countryId != team.countryId
+            && opponent.pool != team.pool
+        }
+    }
+    
+    private var pairingCounts: [Team: Int] {
+        
+        guard draws.pairings.isEmpty == false else { return [:] }
+        
+        var result = [Team: Int]()
+        
+        for opponent in opponents {
+            if let pairing = draws.pairings.first(where: { p in
+                if team.seeded {
+                    return p.seededTeam == team && p.unseededTeam == opponent
+                }
+                
+                return p.unseededTeam == team && p.seededTeam == opponent
+            }) {
+                result[opponent] = pairing.count
             }
         }
+        
+        if result.isEmpty { return [:] }
+        
+        return result
     }
     
-    var drawsCount: Int {
-        pairings.reduce(0) { acc, pairing in
-            acc + pairing.count
+    private var drawsCount: Int {
+        pairingCounts.values.reduce(0) { acc, count in
+            acc + count
         }
-    }
-    
-    private func pairingPercentage(_ pairing: Draws.Pairing) -> Float {
-        Float(pairing.count) / Float(drawsCount) * 100
     }
     
     var body: some View {
@@ -80,21 +97,23 @@ struct TeamDetailView: View {
                     .padding(10)
                     .carded()
                     
-                    if pairings.isEmpty == false {
-                        VStack {
-                            Text("\(drawsCount)")
-                            ForEach(pairings, id: \.self) { pairing in
-                                HStack {
-                                    Text("\(team.seeded ? pairing.unseededTeam.name : pairing.seededTeam.name)")
-                                    Spacer()
-                                    Text("\(pairingPercentage(pairing).rounded().formatted()) % (\(pairing.count))")
+                    VStack {
+                        ForEach(opponents, id: \.self) { opponent in
+                            HStack {
+                                Text(opponent.name)
+                                
+                                Spacer()
+                                
+                                if let count = pairingCounts[opponent] {
+                                    Text("\((Float(count) / Float(drawsCount) * 100).rounded().formatted()) %")
+                                } else {
+                                    Text("no data")
                                 }
                             }
                         }
-                        .padding(15)
-                        .carded()
                     }
-                    
+                    .padding(15)
+                    .carded()
                     
                     Button("draw") {
                         draws.draw(100)
