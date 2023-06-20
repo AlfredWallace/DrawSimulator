9ebeb8160a543a78fb0dc2dfb51a6c8734ed2c6e
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2023-06-20 23:41:18 +0200

    Adding forgotten inverse relationship code  (drawPairings in Season)

--- DrawSimulator/CoreData/Season+CoreDataProperties.swift
+++ DrawSimulator/CoreData/Season+CoreDataProperties.swift
@@ -20,10 +20,16 @@ extension Season {
     @NSManaged public var city: String
     @NSManaged public var stadium: String
     @NSManaged public var seasonTeams: NSSet?
+    @NSManaged public var drawPairings: NSSet?
     
     public var seasonTeamsArray: [SeasonTeam] {
-        let seasonTeamSet = seasonTeams as? Set<SeasonTeam> ?? []
-        return seasonTeamSet.sorted { $0.poolName < $1.poolName }
+        let seasonTeamsSet = seasonTeams as? Set<SeasonTeam> ?? []
+        return seasonTeamsSet.sorted { $0.poolName < $1.poolName }
+    }
+    
+    public var drawPairingsArray: [DrawPairing] {
+        let drawPairingsSet = drawPairings as? Set<DrawPairing> ?? []
+        return drawPairingsSet.sorted { $0.count < $1.count }
     }
 
     @objc public var winYearString: String {
@@ -46,6 +52,23 @@ extension Season {
     @objc(removeSeasonTeams:)
     @NSManaged public func removeFromSeasonTeams(_ values: NSSet)
 
+}
+
+// MARK: Generated accessors for drawPairings
+extension Season {
+    
+    @objc(addDrawPairingsObject:)
+    @NSManaged public func addToDrawPairings(_ value: DrawPairing)
+    
+    @objc(removeDrawPairingsObject:)
+    @NSManaged public func removeFromDrawPairings(_ value: DrawPairing)
+    
+    @objc(addDrawPairings:)
+    @NSManaged public func addToDrawPairings(_ values: NSSet)
+    
+    @objc(removeDrawPairings:)
+    @NSManaged public func removeFromDrawPairings(_ values: NSSet)
+    
 }
 
 extension Season : Identifiable {
