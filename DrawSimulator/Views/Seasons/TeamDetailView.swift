5f0370d73f42ac86994af9071d989d83df845394
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2023-07-10 14:46:33 +0200

    Simplifying the TeamDetailView (too big) by adding a TeamIdentityCardView for the first section of the page

--- DrawSimulator/Views/Seasons/TeamDetailView.swift
+++ DrawSimulator/Views/Seasons/TeamDetailView.swift
@@ -10,37 +10,11 @@ import CoreData
 
 struct TeamDetailView: View {
     
-    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
-    
     @EnvironmentObject private var draws: Draws
-    @EnvironmentObject private var geoSizeTracker: GeoSizeTracker
     @EnvironmentObject private var userSettings: UserSettings
     
     let seasonTeam: SeasonTeam
     let team: Team
-    
-    private var logoSize: CGFloat {
-        var factor = 1.0
-        
-        switch dynamicTypeSize {
-            case .xSmall:
-                factor = 0.36
-            case .small:
-                factor = 0.38
-            case .medium:
-                factor = 0.40
-            case .large:
-                factor = 0.42
-            case .xLarge:
-                factor = 0.44
-            case .xxLarge:
-                factor = 0.46
-            default:
-                factor = 0.75
-        }
-        
-        return geoSizeTracker.getSize().width * factor
-    }
     
     private var opponentsSeasonTeams: [SeasonTeam] {
         seasonTeam.season!.seasonTeamsArray.filter {
@@ -59,35 +33,7 @@ struct TeamDetailView: View {
         
         List {
             // first section is the current team card
-            Section {
-                StackThatFits() {
-                    
-                    Image(team.shortName)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: logoSize, height: logoSize)
-                        .accessibilityHidden(true)
-                    
-                    VStack {
-                        HStack {
-                            Text("Pool")
-                            Image(systemName: "\(seasonTeam.poolName.lowercased()).circle.fill")
-                        }
-                        .accessibilityElement(children: .ignore)
-                        .accessibilityLabel("Pool \(seasonTeam.poolName)")
-                        
-                        DividerView()
-                        
-                        FlagLabelView(team: team)
-                        
-                        DividerView()
-                        
-                        Text(seasonTeam.seededString)
-                    }
-                }
-                .font(.title2.bold())
-            }
-            .listSectionSeparator(.hidden)
+            TeamIdentityCardView(seasonTeam: seasonTeam)
             
             // second section is the list of opponents
             Section {
@@ -137,8 +83,6 @@ struct TeamDetailView: View {
 
 struct TeamDetailView_Previews: PreviewProvider {
     
-    static var geoSizeTracker = GeoSizeTracker()
-    
     static var previews: some View {
         
         let seasonTeam = PreviewDataFetcher.fetchData(
@@ -146,27 +90,18 @@ struct TeamDetailView_Previews: PreviewProvider {
             withPredicate: NSPredicate(format: "team.shortName == %@", DatabaseInitializer.TeamIdentifier.PSG.rawValue)
         )
         
-        return ZStack {
-            GeometryReader { geoWrapper in
-                Spacer()
-                    .onAppear {
-                        geoSizeTracker.setSize(geoWrapper.size)
-                    }
-            }
+        return TabView {
             
-            TabView {
-                
-                NavigationStack {
-                    TeamDetailView(seasonTeam: seasonTeam)
-                        .environment(\.managedObjectContext, CoreDataController.preview.mainContext)
-                        .environmentObject(geoSizeTracker)
-                        .environmentObject(UserSettings())
-                        .environmentObject(Draws(coreDataController: CoreDataController.preview))
-                }
-                .tabItem {
-                    Text("dummy preview tab view")
-                }
+            NavigationStack {
+                TeamDetailView(seasonTeam: seasonTeam)
+                    .environment(\.managedObjectContext, CoreDataController.preview.mainContext)
+                    .environmentObject(UserSettings())
+                    .environmentObject(Draws(coreDataController: CoreDataController.preview))
+            }
+            .tabItem {
+                Text("dummy preview tab view")
             }
         }
+        
     }
 }
---
+++ DrawSimulator/Views/Seasons/TeamIdentityCardView.swift
@@ -0,0 +1,104 @@
+//
+//  TeamIdentityCardView.swift
+//  DrawSimulator
+//
+//  Created by Arthur Falque Pierrotin on 10/07/2023.
+//
+
+import SwiftUI
+
+struct TeamIdentityCardView: View {
+    
+    @EnvironmentObject private var geoSizeTracker: GeoSizeTracker
+    
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
+    
+    let seasonTeam: SeasonTeam
+    let team: Team
+    
+    init(seasonTeam: SeasonTeam) {
+        self.seasonTeam = seasonTeam
+        self.team = seasonTeam.team!
+    }
+    
+    private var logoSize: CGFloat {
+        var factor = 1.0
+        
+        switch dynamicTypeSize {
+            case .xSmall:
+                factor = 0.36
+            case .small:
+                factor = 0.38
+            case .medium:
+                factor = 0.40
+            case .large:
+                factor = 0.42
+            case .xLarge:
+                factor = 0.44
+            case .xxLarge:
+                factor = 0.46
+            default:
+                factor = 0.75
+        }
+        
+        return geoSizeTracker.getSize().width * factor
+    }
+    
+    var body: some View {
+        Section {
+            StackThatFits() {
+                
+                Image(team.shortName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: logoSize, height: logoSize)
+                    .accessibilityHidden(true)
+                
+                VStack {
+                    HStack {
+                        Text("Pool")
+                        Image(systemName: "\(seasonTeam.poolName.lowercased()).circle.fill")
+                    }
+                    .accessibilityElement(children: .ignore)
+                    .accessibilityLabel("Pool \(seasonTeam.poolName)")
+                    
+                    DividerView()
+                    
+                    FlagLabelView(team: team)
+                    
+                    DividerView()
+                    
+                    Text(seasonTeam.seededString)
+                }
+            }
+            .font(.title2.bold())
+        }
+        .listSectionSeparator(.hidden)
+    }
+}
+
+struct TeamIdentityCardView_Previews: PreviewProvider {
+    
+    static var geoSizeTracker = GeoSizeTracker()
+    
+    static var previews: some View {
+        
+        let seasonTeam = PreviewDataFetcher.fetchData(
+            for: SeasonTeam.self,
+            withPredicate: NSPredicate(format: "team.shortName == %@", DatabaseInitializer.TeamIdentifier.PSG.rawValue)
+        )
+        
+        return ZStack {
+            
+            GeometryReader { geoWrapper in
+                Spacer()
+                    .onAppear {
+                        geoSizeTracker.setSize(geoWrapper.size)
+                    }
+            }
+            
+            TeamIdentityCardView(seasonTeam: seasonTeam)
+                .environmentObject(geoSizeTracker)
+        }
+    }
+}
